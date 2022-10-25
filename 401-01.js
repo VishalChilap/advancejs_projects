40f0d9ce3d0dd272aa981a7c146a018d57f7f726
ritikmasand <ritikmasand10@gmail.com>
2022-10-25 20:51:34 +0530

    401 code added

---
+++ 401-01.js
@@ -0,0 +1,10 @@
+// alert('hi ')
+const input=(e)=>{
+    const text=document.getElementById('wrote');
+    const value=(e.target.value);
+      text.innerHTML=value;
+}
+
+
+document.getElementById('input-field').addEventListener('input',input);
+
